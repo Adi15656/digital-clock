f00db58aa3dace08b67fedd5c86c3bb328e12e7f
Adi15656 <ansudolu@gmail.com>
2024-03-02 09:34:12 +0530

    Added Some pages

---
+++ index.js
@@ -0,0 +1,25 @@
+let num = 10;
+var num1 = 10;
+const num2 = 10;
+num = 12;
+
+console.log("this is number:" + num)
+
+console.table(num, num1, num2);
+console.table(num, num1, num2);
+console.table(num, num1, num2);
+console.table(num, num1, num2);
+console.table(num, num1, num2);
+let x = 100;
+let y = 203;
+let z = x + y;
+let m = x - y;
+let p = x/y;
+let pm = x*y;
+
+const z1 = x+y;
+console.table("this is number:" + x,y );
+console.table(m);
+console.table(p);
+console.table(pm);
+
